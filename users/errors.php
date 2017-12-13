114cfc06fb936e9c8ea78146d448bfd25998ad91
andrewscarter <andrew@aktri.com>
2017-12-13 16:00:32 -0500

    [finished] created errors handler for login
    displays if wrong password, invalid email, etc

---
+++ users/errors.php
@@ -0,0 +1,9 @@
+<!--  takes error array and displays in div  -->
+
+<?php  if (count($errors) > 0) : ?>
+  <div class="error">
+  	<?php foreach ($errors as $error) : ?>
+  	  <p><?php echo $error ?></p>
+  	<?php endforeach ?>
+  </div>
+<?php  endif ?>
