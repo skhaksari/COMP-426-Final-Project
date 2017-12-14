eec10f2cefabe122032d2d98951339fd1b3778de
Sufi Khaksari <sarafati@live.unc.edu>
2017-12-13 19:21:52 -0500

    Update EasySki.php

--- EasySki.php
+++ EasySki.php
@@ -8,29 +8,27 @@
         <style type="text/css" media="screen">
 		#calendar-events{ display: none; }
 	   </style>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <link rel="stylesheet" href="EasySki.css">
         <script src="jquery-3.2.1.js" type="text/javascript" charset="utf-8"></script>
-        <script src="jquery-calendar/scripts/calendar.js"></script>
-        
+        <script src="jquery-calendar/scripts/calendar.js"></script>     
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-default navbar-custom">
             <a class="navbar-brand" href="#">EasySki</a>
-             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <span class="navbar-text">
                         Ski Resort Tickets At Your Fingertips
                 </span>
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                    <li class="nav-item" data-target="#favorited">
+                        <a class="nav-link" href="#" data-target="#favorited">Favorites</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -43,13 +41,9 @@
                             <a class="dropdown-item" href="#">Something else here</a>
                         </div>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <button class="btn btn-primary btn-outline-success my-2 my-sm-0" id="signInButton" data-target="#signInFloat">Sign In/Sign Up</button>
                 </form>
             </div>
         </nav>
